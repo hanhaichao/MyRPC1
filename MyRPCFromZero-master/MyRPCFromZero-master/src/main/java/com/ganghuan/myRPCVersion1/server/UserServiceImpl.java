85de6e132948fcb4a7a70ced39e784b0fd1eb926
hanhaichao <2201850471@qq.com>
2022-05-08 19:22:56 +0800

    手写RPC

---
+++ MyRPCFromZero-master/MyRPCFromZero-master/src/main/java/com/ganghuan/myRPCVersion1/server/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ganghuan.myRPCVersion1.server;
+
+import com.ganghuan.myRPCVersion1.common.User;
+import com.ganghuan.myRPCVersion1.service.UserService;
+
+import java.util.Random;
+import java.util.UUID;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User getUserByUserId(Integer id) {
+        System.out.println("客户端查询了"+id+"的用户");
+        // 模拟从数据库中取用户的行为
+        Random random = new Random();
+        User.UserBuilder builder = User.builder();
+        builder.userName(UUID.randomUUID().toString());
+        builder.id(id);
+        builder.sex(random.nextBoolean());
+        User user = builder.build();
+        return user;
+    }
+    @Override
+    public Integer insertUserId(User user) {
+        System.out.println("插入数据成功："+user);
+        return 1;
+    }
+}
